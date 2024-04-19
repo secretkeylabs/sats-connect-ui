36758a58c9e38bc5e2adaa799d0028f72bc6cdfd
Eduard Bardají Puig <ebardaji@gmail.com>
2024-04-19 11:04:04 +0100

    Set explicit text colors

--- src/lib/styles.ts
+++ src/lib/styles.ts
@@ -1,18 +1,21 @@
 import { JSX } from "solid-js/jsx-runtime";
 
 export const titleTextStyles: JSX.CSSProperties = {
+  color: "#181818",
   "font-size": "18px",
   "font-weight": "700",
   "line-height": "1.4",
 };
 
 export const bodyTextStyles: JSX.CSSProperties = {
+  color: "#181818",
   "font-size": "14px",
   "font-weight": "400",
   "line-height": "1.4",
 };
 
 export const buttonTextStyles: JSX.CSSProperties = {
+  color: "#181818",
   "font-size": "14px",
   "font-weight": "500",
 };
